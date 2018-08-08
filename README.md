638f72e00496e85917b642bc6ca2d92e0f44fc8e
supan <supan@aci-bd.com>
2018-08-08 12:45:52 +0600

    Android Grid to List switch first commit...

---
+++ README.md
@@ -0,0 +1,6 @@
+# GridToListSwitch
+
+Android Grid to List & List to Grid switch animation demo
+
+This is a sample showing an easy way to make switch animation from grid layout to list layout at the same time list to grid layout with RecyclerView.
+
---
+++ app/src/main/java/com/sopan/layoutswitch/Item.java
@@ -0,0 +1,31 @@
+package com.sopan.layoutswitch;
+
+public class Item {
+    private int imgResId;
+    private String title;
+    private int likes;
+    private int comments;
+
+    public Item(int imgResId, String title, int likes, int comments) {
+        this.imgResId = imgResId;
+        this.title = title;
+        this.likes = likes;
+        this.comments = comments;
+    }
+
+    public int getImgResId() {
+        return imgResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+}
